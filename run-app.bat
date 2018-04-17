1faf48b26e2cbbfd6449e389bfdff4e1cba5f373
yaHitesh <yahitesh.dev@gmail.com>
2018-04-18 03:10:12 +0530

    MD#5 .bat file added.

---
+++ run-app.bat
@@ -0,0 +1,4 @@
+call mvn clean compile install
+echo "*****************************Application class Execution Start*****************************"
+mvn exec:java
+echo "*****************************Application class Execution Stop*****************************"
